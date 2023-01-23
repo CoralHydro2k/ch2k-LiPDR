beb4debbc0ca429261b992ccc2ec6946b24d7b42
Andrea L. Moore <AndreaLemur@users.noreply.github.com>
2023-01-23 18:29:34 -0500

    Update and rename CH2K_R_Example.R to ch2k_Example.R
    Made ts.tib2 a unique variable to facilitate instances where paleoData_variableName ="_annual|Uncertainty|year" don't need to be, or shouldn't be, filtered out.

--- ch2k_Example.R
+++ ch2k_Example.R
@@ -1,10 +1,10 @@
-# Install lipdR (if necessary; one-time process): ---------------------------------------
+# Install lipdR and geoChronR (if necessary; one-time process): ---------------------------------------
 # install.packages("remotes")
 # library(remotes)
 # remotes::install_github("nickmckay/lipdR")
+# remotes::install_github("nickmckay/geoChronR")
 
 # Load the required packages: ----------------------------------------------------------------
-
 library(lipdR) # To handle LiPD format files
 library(geoChronR) # For plotting
 library(magrittr) # For pipe (%>%)
@@ -12,23 +12,22 @@ library(dplyr) # For data.frame manipulation
 library(ggplot2) # For plotting
 
 # Load the data in: -------------------------------------------------------
-
-# From the lipdverse directly:
+# From the lipdverse directly (v1_0_0 is the current version as of Jan. 2023; check https://lipdverse.org/CoralHydro2k/current_version/ for updates)
 ch2k <- readLipd('https://lipdverse.org/CoralHydro2k/current_version/CoralHydro2k1_0_0.zip')
-# You can also load in a local copy of the .zip file by using readLipd() interactively, or by providing a path with readLipd(path ="[your path]"):
+# You can also load in a local copy of the .zip file interactively, or by providing a path:
 #ch2k <- readLipd()
-
+#ch2k <- readLipd(path = "")
 
 # Exploring the broader database: -------------------------------------------------
-
-# Let's look at the global distribution of records in the database:
+# Let's look at the global distribution of all records in the database:
 mapLipd(ch2k,
         projection = "mollweide",
         global = TRUE)
 
 # We can also plot a simple dashboard summarizing any individual record in the database:
 plotSummary(ch2k[[123]], 
-            paleo.age.var = "year") # Note that paleo.data.var is not supplied here since there are multiple proxy types in the database, but you will be prompted to choose the appropriate variable.
+            paleo.age.var = "year") # Note that paleo.data.var is not supplied here since there are multiple proxy types in the database. 
+                                    # You will be prompted to choose the appropriate variable in the console.
 
 # Convert the multi-LiPD object (ch2k) into a LiPD time series object (TS) for easy manipulation and filtering:
 TS <- extractTs(ch2k)
@@ -67,12 +66,12 @@ summ <- plotSummaryTs(TS,
 # We can convert the lipd-TS to a tibble to filter the database even more efficiently:
 ts.tib <- ts2tibble(TS)
 
-# This filters out values like "d18O_annual", "SrCaUncertainty", and "year" that aren't especially useful to visualize.
-ts.tib <- ts.tib %>%
+# Now filter out values like "d18O_annual", "SrCaUncertainty", and "year" that aren't especially useful to visualize:
+ts.tib2 <- ts.tib %>%
   filter(!grepl("_annual|Uncertainty|year",paleoData_variableName)) 
 
 # Let's look at all of the entries in the CH2K database from the Indian Ocean on a regional map:
-IndOce.tib <- ts.tib %>% # Note that this only takes primary d18O and Sr/Ca time series into account
+IndOce.tib <- ts.tib2 %>% # Note that this only takes primary d18O and Sr/Ca time series into account
   filter(geo_ocean == "Indian Ocean")
 IndOceTS <- as.lipdTs(IndOce.tib) 
 IndOceSumm <- plotSummaryTs(IndOceTS,
@@ -81,14 +80,14 @@ IndOceSumm <- plotSummaryTs(IndOceTS,
                             global = FALSE)
 
 # We can also filter the data to see where (and when) different types of primary proxy measurements are available globally:
-proxy.tib <- ts.tib %>% # Note that this only takes primary d18O and Sr/Ca time series into account
+proxy.tib <- ts.tib2 %>% # Note that this only takes primary d18O and Sr/Ca time series into account
   filter(paleoData_variableName == "d18O" | paleoData_variableName == "SrCa")
 proxySumm <- plotSummaryTs(proxyTS,
                            sort.var = "paleoData_variableName",
                            age.var = "year")
 
 # Filter calls can be combined to narrow down to very specific parameters:
-filter.tib <- ts.tib %>% # Note that this only takes primary d18O and Sr/Ca time series into account
+filter.tib <- ts.tib2 %>% # Note that this only takes primary d18O and Sr/Ca time series into account
   filter(between(geo_latitude,-10,10) & between(geo_longitude,-180,180)) %>% # a specific range of geographic coordinates
   filter(paleoData_archiveSpecies == "Porites lutea") %>% # a specific coral species
   filter(paleoData_coralHydro2kGroup <= 3) %>% # filter for all paired records in the database (Groups 1-3)
@@ -100,7 +99,7 @@ filterSumm <- plotSummaryTs(filterTS,
                             global = TRUE)
 
 # Or slightly less specific ones:
-filter2.tib <- ts.tib %>% # Note that this only takes primary d18O and Sr/Ca time series into account
+filter2.tib <- ts.tib2 %>% # Note that this only takes primary d18O and Sr/Ca time series into account
   filter(grepl("Porites", paleoData_archiveSpecies)) %>% # filter for any coral species containing the string "Porites"
   filter(paleoData_archiveSpecies != "NA") %>%
   filter(paleoData_coralHydro2kGroup <= 3) %>% # filter for all paired records in the database (Groups 1-3)
